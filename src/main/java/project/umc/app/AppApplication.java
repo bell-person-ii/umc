206ef7196ec3e035df4eb6918a93d99013bd2dd7
bell-person-ii <leeji88460@gmail.com>
2023-11-17 23:09:10 +0900

    RestApi 규격형 응답계층 도입

--- src/main/java/project/umc/app/AppApplication.java
+++ src/main/java/project/umc/app/AppApplication.java
@@ -10,6 +10,7 @@ public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/project/umc/app/dto/TempResponse.java
@@ -0,0 +1,16 @@
+package project.umc.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TempResponse {
+
+    private String testString;
+
+}
